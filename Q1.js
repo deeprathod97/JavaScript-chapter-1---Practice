c173fa5d8c45106716b581586c6e0f3f00445d04
deeprathod97 <deeprathod9797@gmail.com>
2024-04-21 21:03:51 +0530

    javascript chapter-1

---
+++ Q1.js
@@ -0,0 +1,33 @@
+// Chapter 1 - Q1
+let a  = "Deep"
+let b = 19
+
+console.log(a + b)
+
+// Chapter 1 - Q2 
+console.log(typeof(a+b))
+
+// Chapter 1 - Q3
+const a1 = {
+    name: "Deep",
+    sectionL: 1,
+    isPrincipal: false
+}
+// a1 = 45
+// a1 = {}
+
+// Chapter 1 - Q4
+
+a['friend'] = "Kunal"
+a['name'] = "Dhruv"
+console.log(a1)
+
+
+// Chapter 1 - Q5
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+  }
+  
+  console.log(dict['yakka'])
